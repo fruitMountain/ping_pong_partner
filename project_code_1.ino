8797a0904b54b924b2a774e499e33d3989b8d16b
seth.berryhill <berr0379@vandals.uidaho.edu>
2016-10-18 14:08:32 -0700

    initial testing for proto-1
    Will spin 2 motors at the same speed, and move 2 servos as a gate for
    ping pong balls.
    
    UNFINISHED

---
+++ project_code_1.ino
@@ -0,0 +1,29 @@
+//seth berryhill
+//oct 13, 2016
+//proto1 code for me223 group project
+
+// code will connect 2 motors constantly spinning at the same speed, controlled with pot
+// and also 2 servos used as a gate to feed out ping pong balls
+
+//include <servo.h>
+
+int motorPin = 4;
+void setup(){
+int motorPin = 4;
+int motor2 = 6;
+int servo1 = 8;
+int servo2 = 10;*/
+
+pinMode(motorPin, OUTPUT);
+/*pinMode(motor2, OUTPUT);
+pinMode(servo1, OUTPUT);
+pinMode(servo2, OUTPUT);
+*/
+}
+
+
+void loop(){
+
+digitalWrite(motorPin, HIGH);
+
+}
